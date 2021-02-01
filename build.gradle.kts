da98c160f8e87503a9dbff2614781955a682e742
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-02-01 08:15:53 +0100

    SubKt: Add common fonts, ktemplate run + delete

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,21 +9,51 @@ plugins {
     id("myaa.subkt")
 }
 
+fun String.isKaraTemplate(): Boolean {
+	return this.startsWith("code") || this.startsWith("template") || this.startsWith("mixin")
+}
+
+fun EventLine.isKaraTemplate(): Boolean {
+	return this.comment && this.effect.isKaraTemplate()
+}
+
 subs {
     readProperties("sub.properties")
     episodes(getList("episodes"))
 
+	val op_ktemplate by task<Automation> {
+        if (file(get("OP")).exists()) {
+            from(get("OP"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
+
+    val ed_ktemplate by task<Automation> {
+        if (file(get("ED")).exists()) {
+            from(get("ED"))
+        }
+
+        video(get("premux"))
+		script("0x.KaraTemplater.moon")
+		macro("0x539's Templater")
+		loglevel(Automation.LogLevel.WARNING)
+	}
+
     merge {
         from(get("dialogue"))
 
-        if (propertyExists("OP")) {
-            from(get("OP")) {
+        if (file(get("OP")).exists()) {
+            from(op_ktemplate.item()) {
                 syncTargetTime(getAs<Duration>("opsync"))
             }
         }
 
-        if (propertyExists("ED")) {
-            from(get("ED")) {
+        if (file(get("ED")).exists()) {
+            from(ed_ktemplate.item()) {
                 syncTargetTime(getAs<Duration>("edsync"))
             }
         }
@@ -39,6 +69,13 @@ subs {
         out(get("mergedname"))
     }
 
+	val cleanmerge by task<ASS> {
+		from(merge.item())
+    	ass {
+			events.lines.removeIf { it.isKaraTemplate() }
+	    }
+	}
+
     chapters {
         from(merge.item())
         chapterMarker("chapter")
@@ -48,23 +85,33 @@ subs {
     mux {
         title(get("title"))
 
-        from(get("premux")) {
+		from(get("premux")) {
+			video {
+				lang("jpn")
+				default(true)
+			}
+			audio {
+				lang("jpn")
+				default(true)
+			}
             includeChapters(false)
 			attachments { include(false) }
-        }
+		}
 
-		from(merge.item()) {
+		from(cleanmerge.item()) {
 			tracks {
 				lang("eng")
                 name(get("group"))
 				default(true)
-				forced(false)
+				forced(true)
 				compression(CompressionType.ZLIB)
 			}
 		}
 
-        chapters(chapters.item()) {
-            lang("eng")
+        chapters(chapters.item()) { lang("eng") }
+
+        attach(get("common_fonts")) {
+            includeExtensions("ttf", "otf")
         }
 
         attach(get("fonts")) {
